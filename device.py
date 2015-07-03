5af31d78175656bc38624cefee5ce1f621bbbd13
yesbut <marouim@gmail.com>
2015-07-02 21:48:15 -0400

    initial

---
+++ device.py
@@ -0,0 +1,29 @@
+__author__ = 'martin'
+# -*- coding: utf-8 -*-
+
+from netconfXML import netconfXML
+from ncclient import manager
+
+
+class Device:
+    __hostname = ""
+    __ipaddress = ""
+    __model = ""
+
+    def __init__(self,
+                 hostname,
+                 ipaddress,
+                 model):
+        self.hostname = hostname
+        self.ipaddress = ipaddress
+        self.model = model
+
+    def pushconfig(self, config):
+        if self.model == 'cisco':
+            print ("Applique configuration: %s - %s - %s" % (self.hostname, self.ipaddress, self.model))
+            xmlstr = netconfXML.get_xml(config)
+            print (xmlstr)
+            m = manager.connect_ssh(self.ipaddress, port=22, username='martin.ouimet', password='&sc1547198', device_params={'name': 'csr'})
+            m.edit_config(target='running', config=xmlstr)
+            m.copy_config(target='startup', source='running')
+
